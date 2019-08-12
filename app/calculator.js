13a11801a3d7d62e3ccfad0e9c68f0050a62c4a9
Lucero EN <luespinozan@gmail.com>
2019-08-12 07:17:42 -0500

    Ejemplos mocha con chai

---
+++ app/calculator.js
@@ -0,0 +1,13 @@
+exports.addTested = function(value) {
+	
+  var result = value + " tested";
+  return result;
+
+};
+
+exports.sum = function(value) {
+	
+  var result = value + 2;
+  return result;
+  
+};
